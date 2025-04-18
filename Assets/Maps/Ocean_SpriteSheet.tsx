a3ae56b8f9020d09a2a0c50664a76a195d869031
Callum Donnelly <callum.donnelly74@gmail.com>
2025-04-18 11:48:27 +0100

    First commit

---
+++ Assets/Maps/Ocean_SpriteSheet.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Ocean_SpriteSheet" tilewidth="32" tileheight="32" tilecount="16" columns="8">
+ <image source="../Images/Ocean_SpriteSheet.png" width="256" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="4" duration="300"/>
+   <frame tileid="5" duration="300"/>
+   <frame tileid="6" duration="300"/>
+   <frame tileid="7" duration="300"/>
+   <frame tileid="8" duration="300"/>
+   <frame tileid="9" duration="300"/>
+   <frame tileid="10" duration="300"/>
+   <frame tileid="11" duration="300"/>
+   <frame tileid="12" duration="300"/>
+   <frame tileid="13" duration="300"/>
+   <frame tileid="14" duration="300"/>
+   <frame tileid="15" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ House.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="House" tilewidth="32" tileheight="32" tilecount="30" columns="6">
+ <image source="WoodenHouse-Pixel/House.png" width="214" height="182"/>
+</tileset>
