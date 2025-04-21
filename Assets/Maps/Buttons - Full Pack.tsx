476da58f225deb95018761d31ea0a43211b7852b
Callum Donnelly <callum.donnelly74@gmail.com>
2025-04-21 21:24:27 +0100

    revision. fixed door event

--- Assets/Maps/Buttons - Full Pack.tsx
+++ Assets/Maps/Buttons - Full Pack.tsx
@@ -1,4 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Buttons - Full Pack" tilewidth="32" tileheight="32" tilecount="384" columns="8">
  <image source="../Images/Buttons - Full Pack.png" width="256" height="1536"/>
+ <tile id="48">
+  <properties>
+   <property name="Door" value="houseowner0"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="Door" value="houseowner1"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="Door" value="houseowner2"/>
+  </properties>
+ </tile>
 </tileset>
